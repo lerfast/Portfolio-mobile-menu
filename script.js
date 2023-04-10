9bebfd9a02c702e1391e96c1ec6e1b5a07070a45
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-04-10 15:49:30 -0500

    Adding Javascript functions

---
+++ script.js
@@ -0,0 +1,11 @@
+function openMenu() {
+    var x = document.getElementById("menu-popup");
+
+      x.style.display = "block";
+    
+  }
+
+function closeMenu() {
+    var x = document.getElementById("menu-popup");
+    x.style.display = "none";
+}
